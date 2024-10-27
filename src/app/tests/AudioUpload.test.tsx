862664e1512d9568caff0a801ef654ab1fbdea85
Sha256_Nulled <Fathindos.FD@gmail.com>
2024-10-27 23:35:19 +0200

    chore: Update test imports for AudioUpload component tests

--- src/app/tests/AudioUpload.test.tsx
+++ src/app/tests/AudioUpload.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import "@testing-library/jest-dom"
 import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import AudioUpload from '../../components/AudioUpload';
 
 describe('AudioUpload Component', () => {
