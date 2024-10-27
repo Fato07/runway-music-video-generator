93ad770b611300783e41b6296d70ebb66562d1fd
Sha256_Nulled <Fathindos.FD@gmail.com>
2024-10-28 01:27:39 +0200

    audio upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import AudioUpload from "@/components/AudioUpload";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        {/* <AudioUpload onFileSelect={}/> */}
         <Image
           className={styles.logo}
           src="/next.svg"
